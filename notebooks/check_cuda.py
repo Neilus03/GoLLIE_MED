32b554963777bf73c980c448b0d4ff123e985ec8
Neilus03 <neildlf@gmail.com>
2024-05-24 17:36:19 +0200

    ff

--- notebooks/check_cuda.py
+++ notebooks/check_cuda.py
@@ -1,3 +1,11 @@
 import torch
-print(torch.cuda.is_available())
-print(torch.version.cuda)
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+print("PyTorch Version:", torch.__version__)
+print("CUDA Available:", torch.cuda.is_available())
+print("CUDA Device Count:", torch.cuda.device_count())
+print("CUDA Device Name:", torch.cuda.get_device_name(0))
+print("CUDA Current Device:", torch.cuda.current_device())
+print("CUDA Device Capability:", torch.cuda.get_device_capability(0))
